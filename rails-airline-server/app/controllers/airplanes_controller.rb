fcdac3c673ba71f1d33049f5104d5013cfefdad4
Philippa Choong <pippachoong@gmail.com>
2022-09-14 00:09:26 +1000

    airplanes seed model view route

---
+++ rails-airline-server/app/controllers/airplanes_controller.rb
@@ -0,0 +1,34 @@
+class AirplanesController < ApplicationController
+
+    def new
+        @airplane = Airplane.new
+    end
+
+    def create
+        Airplane.create airplanes_params
+        redirect_to airplanes_path
+    end
+
+    
+    def index
+        @airplanes = Airplane.all
+    end
+
+    def show
+       Airplane.find params[:id]
+    end
+
+    
+    def destroy
+       Airplane.destroy params[:id]
+       redirect_to airplanes_path
+    end
+
+
+    private
+    def airplanes_params
+        params.permit(:name, :seating_row, :seating_column)
+    end
+
+
+end
--- rails-airline-server/config/routes.rb
+++ rails-airline-server/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # airplanes
+  resources :airplanes #shortcut key to routes
+
 
   # flights 
   get '/flights' => 'flights#index'
