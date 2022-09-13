3ef3fd6e0ba35312867b7698e6396e7e96d36237
Huanyu Li <lihuanyu1024@outlook.com>
2022-09-13 22:08:59 +0930

    flights

---
+++ rails-airline-server/app/controllers/flights_controller.rb
@@ -0,0 +1,24 @@
+class FlightsController < ApplicationController
+
+    def index
+        @flights = Flight.all
+    end
+
+    def create
+        Flight.create flights_params
+        redirect_back(fallback_location:"/flights")
+    end
+
+    def destroy
+       Flight.destroy params[:id]
+       redirect_back(fallback_location:"/flights")
+    end
+
+
+    private
+    def flights_params
+        params.permit(:flight_number, :origin, :destination, :departure_date)
+    end
+
+
+end
--- rails-airline-server/db/seeds.rb
+++ rails-airline-server/db/seeds.rb
@@ -17,7 +17,21 @@
 
 
 # FLIGHTS
+Flight.destroy_all
 
+Flight.create!(
+    flight_number:23,
+    origin:'JFK',
+    destination:'SFO',
+    departure_date:'2013/01/03'
+)
+
+Flight.create!(
+    flight_number:87,
+    origin:'JFK',
+    destination:'LAX',
+    departure_date:'2013/03/03'
+)
 
 
 
@@ -36,6 +50,6 @@
 # RESERVATION
 
 
-_________
+
 
 
