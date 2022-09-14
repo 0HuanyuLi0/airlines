f0521f850b39af36b3a2d4de2d27afc2dc0cea91
Shae Riches <shaeriches@gmail.com>
2022-09-14 11:37:26 +1000

    endLoc & startLoc fields update state

--- airline-frontend/src/components/searchForm.jsx
+++ airline-frontend/src/components/searchForm.jsx
@@ -4,14 +4,23 @@ import React from 'react';
 class SearchForm extends React.Component {
 
     state = {
-        secretText: ''
+        startingLocation: '',
+        endingLocation: ''
     };      //  state
+
+    handleStartLoc = ( event ) => {
+        this.setState({ startingLocation: event.target.value })
+    };      //  handleStartLoc()
+    
+    handleEndLoc = ( event ) => {
+        this.setState({ endingLocation: event.target.value })
+    };      //  handleEndLoc()
 
     handleSubmit = ( event ) => {
         event.preventDefault();         //  Stops form from auto-reloading page on submits
-        console.log(`Form submitted`, this.state.secretText);
+        console.log(`Form submitted`, this.state.startingLocation, this.state.endingLocation);
         
-    };      //  handleSubmit
+    };      //  handleSubmit()
 
     render (){
 
@@ -19,8 +28,8 @@ class SearchForm extends React.Component {
         return(
             <form onSubmit={this.handleSubmit}>
                 <h4>Search Flight</h4>
-                <input type="text" placeholder='Starting Location'/>
-                <input type="text" placeholder='Ending Destination'/>
+                <input type="text" placeholder='Starting Location' onChange={this.handleStartLoc}/>
+                <input type="text" placeholder='Ending Destination' onChange={this.handleEndLoc}/>
                 <button>Search For Flights</button>
             </form>
         );  // return()
