1819629f9af50f09272df2b32ad69b22133b67da
Shae Riches <shaeriches@gmail.com>
2022-09-15 10:11:10 +1000

    made App.js React compatible

--- airline-frontend/src/App.js
+++ airline-frontend/src/App.js
@@ -1,46 +1,53 @@
+import React from 'react';
+
 import './App.css';
 import './css/flights.css'
 import Reservation from './components/reservation';
 import Search from './components/search';
 import {Route, HashRouter as Router, Link, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-      
-        <header>
-          <h1>sei55 - Angel Airlines</h1>
-          <nav>
-            {'\t'}{'\t'}
-            <Link to="/">Home</Link>
-            {'\t'}{'\t'}
-            {'\t'}{'\t'}
-            <Link to="/">Bob</Link>
-            {'\t'}{'\t'}
-          </nav>
-          <hr />
-        </header>
-        
-        
-        
-        <Route component={Search}/>
+class App extends React.Component {
+  
+  render(){
+    
+    return (
 
-
+      <div className="App">
+        <Router>
+        
+          <header>
+            <h1>sei55 - Angel Airlines</h1>
+            <nav>
+              {'\t'}{'\t'}
+              <Link to="/">Home</Link>
+              {'\t'}{'\t'}
+              {'\t'}{'\t'}
+              <Link to="/">Bob</Link>
+              {'\t'}{'\t'}
+            </nav>
+            <hr />
+          </header>
           
-        <Reservation />
+          
+          
+          <Route component={Search}/>
+  
+          <Reservation />
 
 
 
-        <footer>
-          <hr />
-          &copy; sei55 - Angel Airlines.2022
-        </footer>
+          <footer>
+            <hr />
+            &copy; sei55 - Angel Airlines.2022
+          </footer>
 
-      </Router>
+        </Router>
 
-    </div>
-  );
+      </div>
+    );
+  
+  }
+
 }
 
 export default App;
